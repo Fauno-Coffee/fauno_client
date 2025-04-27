e50a648ecaa1ba4398717c6150463c5280d55ac2
Egor Bogatyrev <73602535+runtfint@users.noreply.github.com>
2025-04-28 01:22:23 +0500

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+APP_NAME="fauno_client"
+
+if pm2 describe $APP_NAME > /dev/null; then
+    echo "Stopping existing process..."
+    pm2 stop $APP_NAME
+    pm2 delete $APP_NAME
+fi
+
+echo "Starting new $APP_NAME process"
+NODE_ENV=production pm2 start --name $APP_NAME --watch
+
+pm2 save
