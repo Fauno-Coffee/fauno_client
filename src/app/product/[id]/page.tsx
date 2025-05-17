071e20f6a190d3f6afe09eef295d7e29df983e7a
Егор Богатырёв <egorbogatyrev@MacBook-Pro-Egor.local>
2025-05-18 02:20:33 +0500

    free delivery block

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import s from './page.module.css';
 
@@ -15,201 +15,195 @@ import axios from 'axios';
 import { useCartStore } from '@/shared/store/CartStoreProvider';
 import { CategoryCard } from '@/components/CategoryCard';
 import { BurgerNavbar } from '@/components/BurgerNavbar';
-import { Clouds } from '@/blocks/Clouds';
 import { NotCoffee } from '@/blocks/NotCoffee';
 import { useUserStore } from '@/shared/stores/UserStore/UserStoreProvider';
 import { StickyNavbar } from '@/components/StickyNavbar';
+import { FreeDelivery } from '@/blocks/FreeDelivery';
 
 export default function CatalogPage() {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const [productInfo, setProductInfo] = useState<IProduct>()
+  const [productInfo, setProductInfo] = useState<IProduct>();
 
-    const { openCart } = useCartStore((state) => state)
-    const { user } = useUserStore(state => state);
+  const { openCart } = useCartStore(state => state);
+  const { user } = useUserStore(state => state);
 
-    async function getProduct() {
-        const url = `/product/${id}`;
-        try {
-          const res = await fetch(apiUrlBuilder(url));
-          const data = await res.json();
-          setProductInfo(data);
-        } catch (error) {
-          console.log(error);
-        }
+  async function getProduct() {
+    const url = `/product/${id}`;
+    try {
+      const res = await fetch(apiUrlBuilder(url));
+      const data = await res.json();
+      setProductInfo(data);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async function addToCart() {
-        const url = `/user/plusCart`;
-        try {
-            await axios.post(apiUrlBuilder(url), {
-                session: localStorage.getItem("session"),
-                productId: productInfo?.id
-            });
-            openCart(user.id)
-        } catch (error) {
-          console.log(error);
-        }
+  async function addToCart() {
+    const url = `/user/plusCart`;
+    try {
+      await axios.post(apiUrlBuilder(url), {
+        session: localStorage.getItem('session'),
+        productId: productInfo?.id,
+      });
+      openCart(user.id);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    useEffect(() => {
-        getProduct();
-    }, [id]);
+  useEffect(() => {
+    getProduct();
+  }, [id]);
 
-    return (
-        <div className={s.page}>
-            <Navbar black />
-            <StickyNavbar />
-            <BurgerNavbar />
-            <div className={s.productGridWrapper}>
-                <div className={s.productGrid}>
-                    <div className={s.productImages}>
-                        <Swiper
-                            className={s.swiper}
-                            modules={[Pagination, A11y, Autoplay]}
-                            spaceBetween={0}
-                            slidesPerView={1}
-                            pagination={{ clickable: true }}
-                            loop={true}
-                            autoplay={{ delay: 3000, pauseOnMouseEnter: true }}
-                            speed={1000}
-                        >
-                            {
-                                productInfo?.images.map((image, index) => {
-                                    return (
-                                        <SwiperSlide key={image.imageUrl + index} className={s.image}>
-                                            <Image
-                                                src={imageUrlBuilder(image.imageUrl as string)}
-                                                style={{transform: "scale(1.02)", objectFit: 'cover'}}
-                                                alt='Background'
-                                                layout='fill'
-                                                objectFit='cover'
-                                                objectPosition='center center'
-                                                priority
-                                            />
-                                        </SwiperSlide>
-                                    );
-                                })
-                            }
-                        </Swiper>
-                    </div>
-                    <div className={s.aboutProduct}>
-                        <div className={s.productHeading}>
-                            <h1 className={s.name}>{productInfo?.name}</h1>
-                            <p className={s.description}>{productInfo?.description}</p>
-                        </div> 
-                        <div className={s.feauturesList}>
-                            {
-                                productInfo?.weight &&
-                                <div className={s.feauture}>
-                                    <p className={s.feautureName}>Масса</p>
-                                    <div className={s.divider}></div>
-                                    <p className={s.feautureValue}>{productInfo?.weight}</p>
-                                </div>
-                            }
-                            {
-                                (productInfo?.variation && productInfo.variation.length > 0) &&
-                                <div className={s.feauture}>
-                                    <p className={s.feautureName}>Разновидность</p>
-                                    <div className={s.divider}></div>
-                                    <p className={s.feautureValue}>{productInfo.variation.join(', ')}</p>
-                                </div>
-                            }
-                            {
-                                (productInfo?.processing && productInfo.processing.length > 0) &&
-                                <div className={s.feauture}>
-                                    <p className={s.feautureName}>Обработка</p>
-                                    <div className={s.divider}></div>
-                                    <p className={s.feautureValue}>{productInfo.processing.join(', ')}</p>
-                                </div>
-                            }
-                            {
-                                (productInfo?.fermentation && productInfo.fermentation.length > 0) &&
-                                <div className={s.feauture}>
-                                    <p className={s.feautureName}>Ферментация</p>
-                                    <div className={s.divider}></div>
-                                    <p className={s.feautureValue}>{productInfo.fermentation.join(', ')}</p>
-                                </div>
-                            }
-                            {
-                                productInfo?.region &&
-                                <div className={s.feauture}>
-                                    <p className={s.feautureName}>Регион</p>
-                                    <div className={s.divider}></div>
-                                    <p className={s.feautureValue}>{productInfo?.region}</p>
-                                </div>
-                            }
-                            {
-                                productInfo?.farmer &&
-                                <div className={s.feauture}>
-                                    <p className={s.feautureName}>Фермер</p>
-                                    <div className={s.divider}></div>
-                                    <p className={s.feautureValue}>{productInfo?.farmer}</p>
-                                </div>
-                            }
-                            {
-                                productInfo?.keyDescriptor &&
-                                <div className={s.feauture}>
-                                    <p className={s.feautureName}>Ключевой дескриптор</p>
-                                    <div className={s.divider}></div>
-                                    <p className={s.feautureValue}>{productInfo?.keyDescriptor}</p>
-                                </div>
-                            }
-                        </div>
-                        <div className={s.priceBlock}>
-                            <div className={s.priceWrapper}>
-                                <p className={s.price}>{productInfo?.price} ₽</p>
-                                {(productInfo && productInfo.old_price > 0) && <p className={s.priceOld}>{productInfo?.old_price} ₽</p>}
-                            </div>
-                            <div className={s.buttonWrapper}>
-                                <button className={s.addToCart} onClick={addToCart}>Добавить в корзину</button>
-                            </div>
-                        </div>
-                    </div>
-                    {
-                        productInfo?.about &&
-                        <div className={s.pageBlock}>
-                            <p className={s.blockHeading}>Информация о лоте</p>
-                            <p className={s.productInfo}>{productInfo?.about}</p>
-                        </div>
-                    }
-                    {
-                        productInfo?.recipe &&
-                        <div className={s.pageBlock}>
-                            <p className={s.blockHeading}>Рецепт заваривания</p>
-                            <div className={s.recipe}>
-                                {
-                                    Object.entries(productInfo.recipe).map((data) => {
-                                        if(data[0] != "description"){
-                                            return (
-                                                <p className={s.recipeRow}key={data[0]}>{data[0]}: <span>{data[1]}</span></p>
-                                            )
-                                        }
-                                    })
-                                }
-                                {
-                                    Object.entries(productInfo.recipe).map((data) => {
-                                        if(data[0] == "description"){
-                                            return (
-                                                <p className={s.recipeRowFootnote}key={data[0]}>{data[1]}</p>
-                                            )
-                                        }
-                                    })
-                                }
-                            </div>
-                        </div>
-                    }
-                </div>
-                {
-                    productInfo?.category && 
-                    <div className={s.categoryWrapper}>
-                        <CategoryCard category={productInfo?.category} noButton />
-                    </div>
-                }
+  return (
+    <div className={s.page}>
+      <Navbar black />
+      <StickyNavbar />
+      <BurgerNavbar />
+      <div className={s.productGridWrapper}>
+        <div className={s.productGrid}>
+          <div className={s.productImages}>
+            <Swiper
+              className={s.swiper}
+              modules={[Pagination, A11y, Autoplay]}
+              spaceBetween={0}
+              slidesPerView={1}
+              pagination={{ clickable: true }}
+              loop={true}
+              autoplay={{ delay: 3000, pauseOnMouseEnter: true }}
+              speed={1000}
+            >
+              {productInfo?.images.map((image, index) => {
+                return (
+                  <SwiperSlide key={image.imageUrl + index} className={s.image}>
+                    <Image
+                      src={imageUrlBuilder(image.imageUrl as string)}
+                      style={{ transform: 'scale(1.02)', objectFit: 'cover' }}
+                      alt='Background'
+                      layout='fill'
+                      objectFit='cover'
+                      objectPosition='center center'
+                      priority
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+          <div className={s.aboutProduct}>
+            <div className={s.productHeading}>
+              <h1 className={s.name}>{productInfo?.name}</h1>
+              <p className={s.description}>{productInfo?.description}</p>
             </div>
-            <Clouds />
-            <NotCoffee />
-            <Footer />
+            <div className={s.feauturesList}>
+              {productInfo?.weight && (
+                <div className={s.feauture}>
+                  <p className={s.feautureName}>Масса</p>
+                  <div className={s.divider}></div>
+                  <p className={s.feautureValue}>{productInfo?.weight}</p>
+                </div>
+              )}
+              {productInfo?.variation && productInfo.variation.length > 0 && (
+                <div className={s.feauture}>
+                  <p className={s.feautureName}>Разновидность</p>
+                  <div className={s.divider}></div>
+                  <p className={s.feautureValue}>{productInfo.variation.join(', ')}</p>
+                </div>
+              )}
+              {productInfo?.processing && productInfo.processing.length > 0 && (
+                <div className={s.feauture}>
+                  <p className={s.feautureName}>Обработка</p>
+                  <div className={s.divider}></div>
+                  <p className={s.feautureValue}>{productInfo.processing.join(', ')}</p>
+                </div>
+              )}
+              {productInfo?.fermentation && productInfo.fermentation.length > 0 && (
+                <div className={s.feauture}>
+                  <p className={s.feautureName}>Ферментация</p>
+                  <div className={s.divider}></div>
+                  <p className={s.feautureValue}>{productInfo.fermentation.join(', ')}</p>
+                </div>
+              )}
+              {productInfo?.region && (
+                <div className={s.feauture}>
+                  <p className={s.feautureName}>Регион</p>
+                  <div className={s.divider}></div>
+                  <p className={s.feautureValue}>{productInfo?.region}</p>
+                </div>
+              )}
+              {productInfo?.farmer && (
+                <div className={s.feauture}>
+                  <p className={s.feautureName}>Фермер</p>
+                  <div className={s.divider}></div>
+                  <p className={s.feautureValue}>{productInfo?.farmer}</p>
+                </div>
+              )}
+              {productInfo?.keyDescriptor && (
+                <div className={s.feauture}>
+                  <p className={s.feautureName}>Ключевой дескриптор</p>
+                  <div className={s.divider}></div>
+                  <p className={s.feautureValue}>{productInfo?.keyDescriptor}</p>
+                </div>
+              )}
+            </div>
+            <div className={s.priceBlock}>
+              <div className={s.priceWrapper}>
+                <p className={s.price}>{productInfo?.price} ₽</p>
+                {productInfo && productInfo.old_price > 0 && (
+                  <p className={s.priceOld}>{productInfo?.old_price} ₽</p>
+                )}
+              </div>
+              <div className={s.buttonWrapper}>
+                <button className={s.addToCart} onClick={addToCart}>
+                  Добавить в корзину
+                </button>
+              </div>
+            </div>
+          </div>
+          {productInfo?.about && (
+            <div className={s.pageBlock}>
+              <p className={s.blockHeading}>Информация о лоте</p>
+              <p className={s.productInfo}>{productInfo?.about}</p>
+            </div>
+          )}
+          {productInfo?.recipe && (
+            <div className={s.pageBlock}>
+              <p className={s.blockHeading}>Рецепт заваривания</p>
+              <div className={s.recipe}>
+                {Object.entries(productInfo.recipe).map(data => {
+                  if (data[0] != 'description') {
+                    return (
+                      <p className={s.recipeRow} key={data[0]}>
+                        {data[0]}: <span>{data[1]}</span>
+                      </p>
+                    );
+                  }
+                })}
+                {Object.entries(productInfo.recipe).map(data => {
+                  if (data[0] == 'description') {
+                    return (
+                      <p className={s.recipeRowFootnote} key={data[0]}>
+                        {data[1]}
+                      </p>
+                    );
+                  }
+                })}
+              </div>
+            </div>
+          )}
         </div>
-    );
+        {productInfo?.category && (
+          <div className={s.categoryWrapper}>
+            <CategoryCard category={productInfo?.category} noButton />
+          </div>
+        )}
+      </div>
+
+      <FreeDelivery product={productInfo} />
+
+      <NotCoffee />
+      <Footer />
+    </div>
+  );
 }
---
+++ src/blocks/FreeDelivery/index.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import s from './freeDelivery.module.css';
+import { useEffect, useState } from 'react';
+import { IProduct } from '@/shared/types/Product';
+import { useCartStore } from '@/shared/store/CartStoreProvider';
+import Image from 'next/image';
+import { Button } from '@/shared/ui';
+import axios from 'axios';
+import { apiUrlBuilder } from '@/shared/utils/urlBuilder';
+import { useUserStore } from '@/shared/stores/UserStore/UserStoreProvider';
+import Link from 'next/link';
+
+export const FreeDelivery = ({ product }: { product?: IProduct }) => {
+  const { cart } = useCartStore(state => state);
+
+  const [productInCart, setProductInCart] = useState(false);
+  useEffect(() => {
+    if (product) {
+      const inCartId = cart.findIndex(productInCart => productInCart.productId === product.id);
+      if (inCartId !== -1) setProductInCart(true);
+    }
+  }, [product, cart]);
+
+  const { openCart } = useCartStore(state => state);
+  const { user } = useUserStore(state => state);
+
+  async function addToCart() {
+    const url = `/user/plusCart`;
+    try {
+      await axios.post(apiUrlBuilder(url), {
+        session: localStorage.getItem('session'),
+        productId: product?.id,
+      });
+      openCart(user.id);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <section className={s.delivery_wrapper}>
+      <div className={s.text}>
+        <div className={s.title}>Бесплатная доставка от 3500₽</div>
+        <div className={s.cart_button}>
+          {productInCart ? (
+            <Link href='/catalog'>
+              <Button>Перейти в каталог</Button>
+            </Link>
+          ) : (
+            <Button onClick={addToCart}>Добавить в корзину</Button>
+          )}
+        </div>
+      </div>
+      <div className={s.image}>
+        <Image
+          fill
+          src='/skald.png'
+          alt='Fauno Storage'
+          priority
+          sizes='100%'
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
+    </section>
+  );
+};
